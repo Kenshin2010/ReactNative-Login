f6be24884434bb3976b541352ab254a7560f9a73
Phạm Đoàn <phamdoanmanu38@gmail.com>
2019-01-04 18:03:42 +0700

    commit

--- App.js
+++ App.js
@@ -7,13 +7,48 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,TextInput, Button } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world</Text>
+      <View style={{backgroundColor:'#3F5C9A',alignItems:'center',flex:1}}>
+
+        <Text style={{fontSize:35,color:'#fff',fontWeight:'bold',marginTop:150}}>Facebook</Text>
+
+        <TextInput placeholder='Enter nick name'
+        style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
+        borderTopLeftRadius:5,borderTopRightRadius:5, borderWidth: 0.25, marginTop:50}}
+        onChangeText={(text) => this.setState({text})} />
+
+        <TextInput placeholder='Enter password' textContentType="password" secureTextEntry={true}
+        style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
+        borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
+        onChangeText={(text) => this.setState({text})}/>
+
+        {/* <Button
+         style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10, marginTop:50,
+         borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
+          title="Login"
+          color="#E3E3E3"
+          onPress={() => this._handlePress()}
+          accessibilityLabel="Learn more about this purple button"
+        /> */}
+          
+          {/* text color for button #595D68 */}
+          
+          <View style={[{ width: 300, marginTop: 50, height:45, backgroundColor: "#FF3D00" }]}>
+          <Button
+            style={[{ height:45}]}
+            onPress={this.buttonClickListener}
+            title="Login"
+            color="#FF3D00"
+          />
+        </View>
       </View>
     );
   }
