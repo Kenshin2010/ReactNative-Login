7935ae58d22b0ca29438f8a9e32c6c8bca6f604c
Manroid <phamdoanmanu38@gmail.com>
2019-01-05 09:24:50 +0700

    login test

--- App.js
+++ App.js
@@ -7,52 +7,78 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput, Button } from 'react-native';
+import {Platform, StyleSheet, Text, View,TextInput, Button,TouchableOpacity, Alert } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
+// export default class App extends Component {
+//   constructor(props) { 
+//     super(props);
+//     this.state = { text: '' };
+//   }
+//   render() {
+//     return (
+//       <View style={{backgroundColor:'#3F5C9A',alignItems:'center',flex:1}}>
+
+//         <Text style={{fontSize:35,color:'#fff',fontWeight:'bold',marginTop:150}}>Facebook</Text>
+
+//         <TextInput placeholder='Enter nick name'
+//         style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
+//         borderTopLeftRadius:5,borderTopRightRadius:5, borderWidth: 0.25, marginTop:50}}
+//         onChangeText={(text) => this.setState({text})} />
+
+//         <TextInput placeholder='Enter password' textContentType="password" secureTextEntry={true}
+//         style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
+//         borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
+//         onChangeText={(text) => this.setState({text})}/>
+
+//         {/* <Button
+//          style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10, marginTop:50,
+//          borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
+//           title="Login"
+//           color="#E3E3E3"
+//           onPress={() => this._handlePress()}
+//           accessibilityLabel="Learn more about this purple button"
+//         /> */}
+          
+//           {/* text color for button #595D68 */}
+          
+//           <View style={[{ width: 300, marginTop: 50, backgroundColor: "#FF3D00" }]}>
+//           <Button color='#D6D6D6' title='Login' />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+export default class App extends React.Component {
+  clickMe() {
+    Alert.alert(Platform.select({
+      ios: 'Welcome to iOS!',
+      android: 'Welcome to Android!'
+    }))
   }
   render() {
     return (
-      <View style={{backgroundColor:'#3F5C9A',alignItems:'center',flex:1}}>
-
-        <Text style={{fontSize:35,color:'#fff',fontWeight:'bold',marginTop:150}}>Facebook</Text>
-
-        <TextInput placeholder='Enter nick name'
-        style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
-        borderTopLeftRadius:5,borderTopRightRadius:5, borderWidth: 0.25, marginTop:50}}
-        onChangeText={(text) => this.setState({text})} />
-
-        <TextInput placeholder='Enter password' textContentType="password" secureTextEntry={true}
-        style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10,
-        borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
-        onChangeText={(text) => this.setState({text})}/>
-
-        {/* <Button
-         style={{backgroundColor: 'white',height: 45, width:300, borderColor: '#E3E3E3', paddingLeft:10, marginTop:50,
-         borderBottomLeftRadius:5,borderBottomRightRadius:5, borderWidth: 0.25}}
-          title="Login"
-          color="#E3E3E3"
-          onPress={() => this._handlePress()}
-          accessibilityLabel="Learn more about this purple button"
-        /> */}
-          
-          {/* text color for button #595D68 */}
-          
-          <View style={[{ width: 300, marginTop: 50, height:45, backgroundColor: "#FF3D00" }]}>
-          <Button
-            style={[{ height:45}]}
-            onPress={this.buttonClickListener}
-            title="Login"
-            color="#FF3D00"
-          />
+      <TouchableOpacity onPress={this.clickMe()}>
+        <View style={styles.box}>
+          <Text>Hello {this.props.name}. Please click me.</Text>
         </View>
-      </View>
+      </TouchableOpacity>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  box: {
+    borderColor: 'red',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    padding: 10,
+    width: 100,
+    height: 100
+  }
+});
+
+
 
 const styles = StyleSheet.create({
   container: {
